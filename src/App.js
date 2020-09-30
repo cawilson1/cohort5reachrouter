153063db00de04dfaf8a87227ef9089b8aa68d6f
cawilson1 <cawilson1@g.cofc.edu>
2020-09-30 10:20:29 -0400

    added routes for home and dashboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "@reach/router";
+import { Link, Router } from "@reach/router";
 
 function App() {
   return (
@@ -7,8 +7,27 @@ function App() {
       <nav>
         <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
       </nav>
+      Hey I am here
+      <Router>
+        <Home path="/" />
+        <Dashboard path="/dashboard" />
+      </Router>
     </div>
   );
 }
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome</h2>
+    </div>
+  );
+}
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+);
 
 export default App;
