f65d74a3a543183988c2cb022d098a4a8b3818d5
cawilson1 <cawilson1@g.cofc.edu>
2020-09-30 11:07:51 -0400

    navigate to invoice based on user input

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const Invoice = ({ invoiceId }) => {
   );
 };
 
-const Invoices = ({ children }) => {
+const Invoices = ({ children, navigate }) => {
   return (
     <div>
       <h2>Invoices</h2>
@@ -61,6 +61,23 @@ const Invoices = ({ children }) => {
           <Link to="75">Invoice 75</Link>
         </li>
       </ul>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          console.log(event.target.elements[0].value);
+          const id = event.target.elements[0].value;
+          event.target.reset();
+          navigate(id);
+        }}
+      >
+        <p>
+          <label>
+            New Invoice ID:
+            <input type="text" name="invId" />
+          </label>
+          <button type="submit">Create new invoice</button>
+        </p>
+      </form>
       {children}
     </div>
   );
